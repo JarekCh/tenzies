81e5ac8da0728830fe4bb92ddd3090b15da5cb62
Jaroslaw Charchula <charchula@gmail.com>
2022-06-28 17:37:00 +0200

    add allNewDice func

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import React, {useEffect, useState} from 'react';
 import Die from './components/Die'
 
 function App() {
-  const [value, setValue] = useState('1')
+  const [value, setValue] = useState('1');
+
+  function allNewDice() {
+    const diceArray = []
+
+    for(let i = 0; i <= 9; i++) {
+      diceArray.push(Math.floor(Math.random() * 6) + 1)
+    }
+    console.log(diceArray);
+  }
 
   return (
     <main className="main">     
@@ -19,7 +28,7 @@ function App() {
         <Die value={value} />
         <Die value={value} />
       </div>      
-      {/* button */}       
+      <button onClick={allNewDice}>generate number</button>     
     </main>
   );
 }
